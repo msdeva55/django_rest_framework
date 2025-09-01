dfb13236d3ae255c17524a65709715f6a831ddd6
msdeva55 <msdevendhiran@gmail.com>
2025-09-01 11:59:10 +0530

    Initial commit

---
+++ SchoolManagement/settings.py
@@ -0,0 +1,110 @@
+
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ytmy)rx1r3ugx@m25m2jj&h=g=b(btj986&15e0v#ol-&0)ua+'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'Students',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'SchoolManagement.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'SchoolManagement.wsgi.application'
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'school_db',
+        'USER' : 'root',
+        'PASSWORD' : 'admin',
+        'HOST' :'localhost',
+        'PORT' : '3306',
+    }
+}
+
+
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+
+
+STATIC_URL = 'static/'
+
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ Students/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class SampleTable(models.Model):
+
+    age = models.IntegerField(default=10)
+    weight = models.FloatField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    name = models.CharField(max_length=150)
+    feedback = models.TextField()
+
+    date_of_birth = models.DateField(auto_now=True)
+    date_of_birth_with_time = models.DateTimeField()
+    birth_time = models.TimeField()
+
+    is_admin = models.BooleanField()
